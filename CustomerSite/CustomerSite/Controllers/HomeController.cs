332943186da6cd2382686f261ed202cba8c48d51
giavanxilin123 <1614058@hcmut.edu.vn>
2022-06-28 09:54:22 +0700

    add product api & fetch product

--- CustomerSite/CustomerSite/Controllers/HomeController.cs
+++ CustomerSite/CustomerSite/Controllers/HomeController.cs
@@ -1,119 +1,119 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using CustomerSite.Models;
-using CustomerSite.Helper;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Text;
+﻿// using System.Diagnostics;
+// using Microsoft.AspNetCore.Mvc;
+// using CustomerSite.Models;
+// using CustomerSite.Helper;
+// using Newtonsoft.Json;
+// using System.Collections.Generic;
+// using System.Text;
 
-namespace CustomerSite.Controllers{
+// namespace CustomerSite.Controllers{
 
-    public class HomeController : Controller
-    {
-        APIHelper _api = new APIHelper();
+//     public class HomeController : Controller
+//     {
+//         APIHelper _api = new APIHelper();
 
-        public async Task<IActionResult> Index()
-        {
-            ViewBag.Id = 1;
-            ViewBag.Name = "";
-            ViewBag.Age = 0;
-            ViewBag.Role = "";
-            //ViewBag.Created = DateTime.Now.AddDays(0).ToString("dd-MM-yyyy");
-            ViewBag.Created = DateTime.Now.ToUniversalTime().ToString("s") + "Z";
-            ViewBag.PhoneNumber = "";
+//         public async Task<IActionResult> Index()
+//         {
+//             ViewBag.Id = 1;
+//             ViewBag.Name = "";
+//             ViewBag.Age = 0;
+//             ViewBag.Role = "";
+//             //ViewBag.Created = DateTime.Now.AddDays(0).ToString("dd-MM-yyyy");
+//             ViewBag.Created = DateTime.Now.ToUniversalTime().ToString("s") + "Z";
+//             ViewBag.PhoneNumber = "";
 
-            Admin list = new Admin();
+//             Admin list = new Admin();
             
-            List<Admin> data = new List<Admin>();
-            HttpClient client = _api.initial();
+//             List<Admin> data = new List<Admin>();
+//             HttpClient client = _api.initial();
 
-            HttpResponseMessage res = await client.GetAsync("api/admin/");
-            if (res.IsSuccessStatusCode)
-            {
-                var result = res.Content.ReadAsStringAsync().Result;
-                data = JsonConvert.DeserializeObject<List<Admin>>(result);
+//             HttpResponseMessage res = await client.GetAsync("api/admin/");
+//             if (res.IsSuccessStatusCode)
+//             {
+//                 var result = res.Content.ReadAsStringAsync().Result;
+//                 data = JsonConvert.DeserializeObject<List<Admin>>(result);
 
             
-                data.ForEach(d =>
-                {
-                    list.admin.Add(new Admin
-                    {
-                        Name = d.Name,
-                        Id = d.Id,
-                        Role = d.Role,
-                        Age = d.Age,
-                        PhoneNumber = d.PhoneNumber,
-                        Created = d.Created
-                    });
-                });
-            }
+//                 data.ForEach(d =>
+//                 {
+//                     list.admin.Add(new Admin
+//                     {
+//                         Name = d.Name,
+//                         Id = d.Id,
+//                         Role = d.Role,
+//                         Age = d.Age,
+//                         PhoneNumber = d.PhoneNumber,
+//                         Created = d.Created
+//                     });
+//                 });
+//             }
 
-            List<Admin> model = list.admin.ToList();
+//             List<Admin> model = list.admin.ToList();
             
-            return View(model);
-        }
+//             return View(model);
+//         }
 
 
-        [HttpPost]
-        public async Task<ActionResult<Admin>> AddAdmin(Admin model)
-        {
-            HttpClient client = _api.initial();
-            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
-            var action = "api/admin";
-            if (model.Id == 0)
-            {
-                HttpResponseMessage res = await client.PostAsync(action, content).ConfigureAwait(false);
-                res.EnsureSuccessStatusCode();
-                if (res.IsSuccessStatusCode)
-                {
-                    var result = res.Content.ReadAsStringAsync().Result;
-                }
-                return RedirectToAction("Index", "Home");
-            }
+//         [HttpPost]
+//         public async Task<ActionResult<Admin>> AddAdmin(Admin model)
+//         {
+//             HttpClient client = _api.initial();
+//             var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+//             var action = "api/admin";
+//             if (model.Id == 0)
+//             {
+//                 HttpResponseMessage res = await client.PostAsync(action, content).ConfigureAwait(false);
+//                 res.EnsureSuccessStatusCode();
+//                 if (res.IsSuccessStatusCode)
+//                 {
+//                     var result = res.Content.ReadAsStringAsync().Result;
+//                 }
+//                 return RedirectToAction("Index", "Home");
+//             }
             
-            else
-            {
-                action = "api/admin/id?id=" + model.Id;
-                HttpResponseMessage res = await client.PutAsync(action, content).ConfigureAwait(false);
-                res.EnsureSuccessStatusCode();
-                if (res.IsSuccessStatusCode)
-                {
-                    var result = res.Content.ReadAsStringAsync().Result;
-                }
-                return RedirectToAction("Index", "Home");
-            }
+//             else
+//             {
+//                 action = "api/admin/id?id=" + model.Id;
+//                 HttpResponseMessage res = await client.PutAsync(action, content).ConfigureAwait(false);
+//                 res.EnsureSuccessStatusCode();
+//                 if (res.IsSuccessStatusCode)
+//                 {
+//                     var result = res.Content.ReadAsStringAsync().Result;
+//                 }
+//                 return RedirectToAction("Index", "Home");
+//             }
 
-        }
+//         }
 
 
-        [HttpPost, ActionName("Delete")]
-        public async Task<ActionResult<Admin>> Delete(Admin model)
-        {
+//         [HttpPost, ActionName("Delete")]
+//         public async Task<ActionResult<Admin>> Delete(Admin model)
+//         {
 
-            Console.WriteLine("delete");
-            HttpClient client = _api.initial();
-            Console.WriteLine("alo" + model.Id);
-            //var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
-            var action = "api/admin/id?id=" + model.Id;
-            HttpResponseMessage res = await client.DeleteAsync(action).ConfigureAwait(false);
-            res.EnsureSuccessStatusCode();
-            //if (res.IsSuccessStatusCode)
-            //{
-            //    var result = res.Content.ReadAsStringAsync().Result;
-            //}
-            return RedirectToAction("Index", "Home");
-        }
+//             Console.WriteLine("delete");
+//             HttpClient client = _api.initial();
+//             Console.WriteLine("alo" + model.Id);
+//             //var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+//             var action = "api/admin/id?id=" + model.Id;
+//             HttpResponseMessage res = await client.DeleteAsync(action).ConfigureAwait(false);
+//             res.EnsureSuccessStatusCode();
+//             //if (res.IsSuccessStatusCode)
+//             //{
+//             //    var result = res.Content.ReadAsStringAsync().Result;
+//             //}
+//             return RedirectToAction("Index", "Home");
+//         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+//         public IActionResult Privacy()
+//         {
+//             return View();
+//         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-}
+//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//         public IActionResult Error()
+//         {
+//             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//         }
+//     }
+// }
 
--- CustomerSite/CustomerSite/Pages/Index.cshtml.cs
+++ CustomerSite/CustomerSite/Pages/Index.cshtml.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CustomerSite.Helper;
+using CustomerSite.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CustomerSite.Pages
 {
 	public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        // private HttpClient _client;
+        private APIHelper _api = new APIHelper();
+       
+        public List<Product> data = new List<Product>();
+
+        public async Task<IActionResult> OnGetAsync(){
+            HttpClient client = _api.initial();
+            var response = await client.GetAsync("api/product/");
+            var result =  response.Content.ReadAsStringAsync().Result;
+            data = JsonConvert.DeserializeObject<List<Product>>(result);
+            return Page();
         }
+        
     }
 }
